77e575b0b1aedc10a068d8557dd55569244396e0
Vincent <nacen-dev@gmail.com>
2022-05-21 03:22:33 +0800

    feat:(product) createNewProduct function

--- src/service/productService.ts
+++ src/service/productService.ts
@@ -1,8 +1,12 @@
-import { ProductModel } from "../model/productModel"
+import { Product, ProductModel } from "../model/productModel"
 
 export const getAllProducts = (category?: string) => {
   if (category) {
     return ProductModel.find({category: category})
   }
   return ProductModel.find();
+}
+
+export const createNewProduct = (product: Partial<Product>) => {
+  return ProductModel.create(product);
 }
