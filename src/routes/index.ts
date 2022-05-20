5983ff387403a8a5745060cb61658f91ec261c8d
nacen <nacen.dev@gmail.com>
2022-05-20 17:04:34 +0800

    feat:(routing) use productRouter in indexRouter

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { authRouter } from "./authRoutes";
+import { productRouter } from "./productRoutes";
 import { userRouter } from "./userRoutes";
 
 export const indexRouter = express.Router();
@@ -8,3 +9,4 @@ indexRouter.get("/healthcheck", (req, res) => res.sendStatus(200));
 
 indexRouter.use(userRouter);
 indexRouter.use(authRouter);
+indexRouter.use(productRouter);
