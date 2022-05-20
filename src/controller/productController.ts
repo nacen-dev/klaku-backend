b7cb4d6ade86cf112d1a1b1d055e9991d1d7143a
Vincent <nacen-dev@gmail.com>
2022-05-21 03:24:55 +0800

    feat:(product) createNewProductHandler

--- src/controller/productController.ts
+++ src/controller/productController.ts
@@ -1,15 +1,23 @@
 import { Request, Response } from "express";
-import { getAllProducts } from "../service/productService";
+import { createNewProduct, getAllProducts } from "../service/productService";
 
 export const getAllProductsHandler = async (req: Request, res: Response) => {
   try {
     const queryCategory = req.query.category ? req.query.category : "";
-    let products;
-    
-    products = await getAllProducts(queryCategory as string);
+    const products = await getAllProducts(queryCategory as string);
 
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json(error);
   }
 };
+
+export const createNewProductHandler = async (req: Request, res: Response) => {
+  const body = req.body;
+  try { 
+    const product = await createNewProduct(body);
+    return res.send("Product successfully created")
+  } catch(error) {
+    res.status(500).json(error)
+  }
+}
