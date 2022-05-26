0c63c73ffedddfa3cf3d5d41ce03cca8d1a9ea26
Vincent <nacen.dev@gmail.com>
2022-05-27 01:08:12 +0800

    feat: getAllOrders query

--- src/service/orderService.ts
+++ src/service/orderService.ts
@@ -4,3 +4,7 @@ export const getAllOrdersByUserId = (userId: string) => {
   console.log(userId);
   return OrderModel.find({user: userId});
 }
+
+export const getAllOrders = () => {
+  return OrderModel.find();
+}
