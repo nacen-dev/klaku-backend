59b0b58eca4c9e2cfee169966b268069adebd5f5
Vincent <nacen.dev@gmail.com>
2022-05-27 03:15:46 +0800

    feat: requireAdmin for create, update, delete

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -1,17 +1,39 @@
-import express from 'express';
-import { getAllProductsHandler, createNewProductHandler, updateProductByIdHandler, deleteProductByIdHandler } from '../controller/productController';
-import { validateResource } from '../middleware/validateResource';
-import { verifyProductIdSchema, verifyProductSchema } from '../schema/productSchema';
+import express from "express";
+import {
+  getAllProductsHandler,
+  createNewProductHandler,
+  updateProductByIdHandler,
+  deleteProductByIdHandler,
+} from "../controller/productController";
+import { requireAdmin } from "../middleware/requireAdmin";
+import { validateResource } from "../middleware/validateResource";
+import {
+  verifyProductIdSchema,
+  verifyProductSchema,
+} from "../schema/productSchema";
 
 export const productRouter = express.Router();
 
-productRouter.get("/api/products/", getAllProductsHandler)
+productRouter.get("/api/products/", getAllProductsHandler);
 
-productRouter.post("/api/products/new", validateResource(verifyProductSchema), createNewProductHandler);
+productRouter.post(
+  "/api/products/new",
+  requireAdmin,
+  validateResource(verifyProductSchema),
+  createNewProductHandler
+);
 
-productRouter.put("/api/products/:productId", 
-  validateResource(verifyProductIdSchema), 
-  validateResource(verifyProductSchema), 
-  updateProductByIdHandler)
+productRouter.put(
+  "/api/products/:productId",
+  requireAdmin,
+  validateResource(verifyProductIdSchema),
+  validateResource(verifyProductSchema),
+  updateProductByIdHandler
+);
 
-productRouter.delete("/api/products/:productId", validateResource(verifyProductIdSchema), deleteProductByIdHandler)
+productRouter.delete(
+  "/api/products/:productId",
+  requireAdmin,
+  validateResource(verifyProductIdSchema),
+  deleteProductByIdHandler
+);
