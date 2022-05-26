46e22337f1c2019c8ef82b646ab9e3b4b3009402
Vincent <nacen.dev@gmail.com>
2022-05-27 03:14:39 +0800

    feat: use getOrderById in orderRoutes

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -2,11 +2,16 @@ import express from "express";
 import {
   getAllOrdersByUserIdHandler,
   getAllOrdersHandler,
+  getOrderByIdHandler,
   updateOrderHandler,
 } from "../controller/orderController";
 import { validateResource } from "../middleware/validateResource";
 import { requireAdmin } from "../middleware/requireAdmin";
-import { verifyOrderUpdateSchema } from "../schema/orderSchema";
+import {
+  verifyOrderIdSchema,
+  verifyOrderUpdateSchema,
+} from "../schema/orderSchema";
+import { requireUser } from "../middleware/requireUser";
 
 export const orderRouter = express.Router();
 
@@ -17,4 +22,11 @@ orderRouter.put(
   requireAdmin,
   validateResource(verifyOrderUpdateSchema),
   updateOrderHandler
+);
+
+orderRouter.get(
+  "/api/orders/:orderId",
+  requireUser,
+  validateResource(verifyOrderIdSchema),
+  getOrderByIdHandler
 );
