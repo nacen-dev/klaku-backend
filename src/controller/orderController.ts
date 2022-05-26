4b1c8925a06812be8b22253556626c66dd2fd47b
Vincent <nacen.dev@gmail.com>
2022-05-27 01:08:32 +0800

    feat: use getAllOrdersQuery

--- src/controller/orderController.ts
+++ src/controller/orderController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { VerifyUserIdInput } from "../schema/userSchema";
-import { getAllOrdersByUserId } from "../service/orderService";
+import { getAllOrders, getAllOrdersByUserId } from "../service/orderService";
 
 export const getAllOrdersByUserIdHandler = async (req: Request<VerifyUserIdInput>, res: Response) => {
   try {
@@ -8,5 +8,14 @@ export const getAllOrdersByUserIdHandler = async (req: Request<VerifyUserIdInput
     return res.status(200).json(orders);
   } catch (error) {
     return res.status(500).json(error);
+  }
+}
+
+export const getAllOrdersHandler = async (req: Request, res: Response) => {
+  try {
+    const orders = await getAllOrders();
+    return res.status(200).json(orders);
+  } catch (error) {
+    return res.status(500).json(error)
   }
 }
