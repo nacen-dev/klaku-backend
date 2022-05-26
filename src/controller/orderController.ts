b470501b37ead15eb74e8ebaf9f37fa3eed46a89
Vincent <nacen.dev@gmail.com>
2022-05-27 02:14:42 +0800

    feat: create updateOrderHandler

--- src/controller/orderController.ts
+++ src/controller/orderController.ts
@@ -1,21 +1,42 @@
 import { Request, Response } from "express";
+import {
+  VerifyOrderIdInput,
+  VerifyOrderUpdateInput,
+} from "../schema/orderSchema";
 import { VerifyUserIdInput } from "../schema/userSchema";
-import { getAllOrders, getAllOrdersByUserId } from "../service/orderService";
+import { getAllOrders, getAllOrdersByUserId, updateOrder } from "../service/orderService";
 
-export const getAllOrdersByUserIdHandler = async (req: Request<VerifyUserIdInput>, res: Response) => {
+export const getAllOrdersByUserIdHandler = async (
+  req: Request<VerifyUserIdInput>,
+  res: Response
+) => {
   try {
     const orders = await getAllOrdersByUserId(req.params.userId);
     return res.status(200).json(orders);
   } catch (error) {
     return res.status(500).json(error);
   }
-}
+};
 
 export const getAllOrdersHandler = async (req: Request, res: Response) => {
   try {
     const orders = await getAllOrders();
     return res.status(200).json(orders);
   } catch (error) {
-    return res.status(500).json(error)
+    return res.status(500).json(error);
   }
-}
+};
+
+export const updateOrderHandler = async (
+  req: Request<VerifyOrderIdInput, {}, VerifyOrderUpdateInput>,
+  res: Response
+) => {
+  const orderId = req.params.orderId;
+  const updateData = req.body;
+  try {
+    const order = await updateOrder(orderId, updateData);
+    res.status(200).json(order);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
