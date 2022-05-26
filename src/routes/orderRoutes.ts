910f84d3343fc800b24b78e2681279af5bbf0834
Vincent <nacen.dev@gmail.com>
2022-05-27 02:35:31 +0800

    fix: get all orders only for admins

--- src/routes/orderRoutes.ts
+++ src/routes/orderRoutes.ts
@@ -6,11 +6,10 @@ import {
 } from "../controller/orderController";
 import { validateResource } from "../middleware/validateResource";
 import { verifyUserIdSchema } from "../schema/userSchema";
-import { requireUser } from "../middleware/requireUser";
 import { requireAdmin } from "../middleware/requireAdmin";
 
 export const orderRouter = express.Router();
 
-orderRouter.get("/api/orders", requireUser, getAllOrdersHandler);
+orderRouter.get("/api/orders", requireAdmin, getAllOrdersHandler);
 
 orderRouter.put("/api/orders/:orderId", requireAdmin, updateOrderHandler);
