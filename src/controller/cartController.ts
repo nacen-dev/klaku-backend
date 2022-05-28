72e9cace9ee1ff77ebff265f3bf5e2d7a53ed2d0
Vincent <nacen.dev@gmail.com>
2022-05-28 20:34:02 +0800

    feat: updateCartHandler

--- src/controller/cartController.ts
+++ src/controller/cartController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
+import { Cart } from "../model/cartModel";
+import { verifyCartInput } from "../schema/cartSchema";
 import { VerifyUserIdInput } from "../schema/userSchema";
-import { getCartByUserId } from "../service/cartService";
+import { getCartByUserId, updateCartByUserId } from "../service/cartService";
 
 export const getCartByUserIdHandler = async (
   req: Request<VerifyUserIdInput>,
@@ -8,13 +10,21 @@ export const getCartByUserIdHandler = async (
 ) => {
   try {
     const userId = req.params.userId;
-
-    if (userId !== res.locals.user._id)
-      return res
-        .status(403)
-        .json({ error: "Requested user's cart does not match the user that is currently logged in" });
-
     const cart = await getCartByUserId(userId);
+    return res.status(200).json(cart);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+};
+
+export const updateCartByUserIdHandler = async (
+  req: Request<VerifyUserIdInput, {}, verifyCartInput>,
+  res: Response
+) => {
+  try {
+    const userId = req.params.userId;
+    const cartData = req.body;
+    const cart = await updateCartByUserId(userId, cartData as unknown as Partial<Cart>);
     return res.status(200).json(cart);
   } catch (error) {
     return res.status(500).json(error);
