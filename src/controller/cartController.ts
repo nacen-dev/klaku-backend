5d3a40e69aac086f26b19cd67a7a90589d3074ec
Vincent <nacen.dev@gmail.com>
2022-05-28 19:44:31 +0800

    feat: add a check if requester matches cart's user

--- src/controller/cartController.ts
+++ src/controller/cartController.ts
@@ -8,6 +8,12 @@ export const getCartByUserIdHandler = async (
 ) => {
   try {
     const userId = req.params.userId;
+
+    if (userId !== res.locals.user._id)
+      return res
+        .status(403)
+        .json({ error: "Requested user's cart does not match the user that is currently logged in" });
+
     const cart = await getCartByUserId(userId);
     return res.status(200).json(cart);
   } catch (error) {
