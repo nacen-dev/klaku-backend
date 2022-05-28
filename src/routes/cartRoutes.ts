b7e332de0bf1a0be2cbe9c18e8e32fd0b9628dc6
Vincent <nacen.dev@gmail.com>
2022-05-28 20:34:25 +0800

    feat: update cart route

--- src/routes/cartRoutes.ts
+++ src/routes/cartRoutes.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { getCartByUserIdHandler } from "../controller/cartController";
+import { getCartByUserIdHandler, updateCartByUserIdHandler } from "../controller/cartController";
+import { checkIfSameUser } from "../middleware/checkIfSameUser";
 import { requireUser } from "../middleware/requireUser";
 import { validateResource } from "../middleware/validateResource";
+import { verifyCartSchema } from "../schema/cartSchema";
 import { verifyUserIdSchema } from "../schema/userSchema";
 
 export const cartRouter = express.Router();
@@ -9,6 +11,16 @@ export const cartRouter = express.Router();
 cartRouter.get(
   "/api/cart/:userId",
   requireUser,
+  checkIfSameUser,
   validateResource(verifyUserIdSchema),
   getCartByUserIdHandler
+);
+
+cartRouter.put(
+  "/api/cart/:userId",
+  requireUser,
+  checkIfSameUser,
+  validateResource(verifyUserIdSchema),
+  validateResource(verifyCartSchema),
+  updateCartByUserIdHandler
 );
