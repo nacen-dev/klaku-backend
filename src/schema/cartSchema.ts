1ac1b692e00e71e641caef4ffb4e782f73632c04
Vincent <nacen.dev@gmail.com>
2022-05-28 21:30:58 +0800

    feat: add productId validation, min quantity

--- src/schema/cartSchema.ts
+++ src/schema/cartSchema.ts
@@ -3,10 +3,13 @@ import { object, string, number, TypeOf } from "zod";
 export const verifyCartSchema = object({
   body: object({
     items: object({
-      productId: string({required_error: "Product ID is required"}),
-      quantity: number({required_error: "Quantity is required"})
-    }).array()
-  }).strict()
-})
+      productId: string({ required_error: "Product ID is required" }).regex(
+        /^[0-9a-fA-F]{24}$/,
+        { message: "Invalid format" }
+      ),
+      quantity: number({ required_error: "Quantity is required" }).min(1),
+    }).array(),
+  }).strict(),
+});
 
 export type verifyCartInput = TypeOf<typeof verifyCartSchema>["body"];
