a7164d1d097c715b731eee803256756d26d352b2
Vincent <nacen.dev@gmail.com>
2022-05-28 22:39:11 +0800

    feat: add product and stock validation
    Checks if every product is a valid product.
    Also checked if there is enough stock for each quantity provided per item

--- src/controller/cartController.ts
+++ src/controller/cartController.ts
@@ -3,6 +3,7 @@ import { Cart } from "../model/cartModel";
 import { verifyCartInput } from "../schema/cartSchema";
 import { VerifyUserIdInput } from "../schema/userSchema";
 import { getCartByUserId, updateCartByUserId } from "../service/cartService";
+import { getProductById } from "../service/productService";
 
 export const getCartByUserIdHandler = async (
   req: Request<VerifyUserIdInput>,
@@ -25,10 +26,33 @@ export const updateCartByUserIdHandler = async (
   try {
     const userId = req.params.userId;
     const cartData = req.body;
+    const productsData = cartData.items.map((item) =>
+      getProductById(item.productId)
+    );
+    const productsInCart = await Promise.all(productsData);
+
+    // Check if every product in cart is a valid product
+    if (!productsInCart.every((product) => product !== null)) {
+      return res.status(404).send("Invalid products provided");
+    }
+
+    // Check if there is enough stock before updating cart data
+
+    const checkIfEnoughStock = !productsInCart
+      .map((product, index) => {
+        return product && product.stock >= cartData.items[index].quantity;
+      })
+      .some((product) => product === false);
+
+    if (!checkIfEnoughStock) {
+      return res.status(400).json({ error: "Not enough stock" });
+    }
+
     const cart = await updateCartByUserId(
       userId,
       cartData as unknown as Partial<Cart>
     );
+
     return res.status(200).json(cart);
   } catch (error) {
     return res.status(500).json(error);
