789eb81d394b2c4435298af1cd679f5362bc7792
Vincent <nacen.dev@gmail.com>
2022-05-28 19:24:39 +0800

    feat: getCartByUserId query

--- src/service/cartService.ts
+++ src/service/cartService.ts
@@ -3,3 +3,7 @@ import { CartModel } from "../model/cartModel";
 export const createCart = (userId: string) => {
   return CartModel.create({ user: userId });
 };
+
+export const getCartByUserId = (userId: string) => {
+  return CartModel.find({ user: userId });
+};
