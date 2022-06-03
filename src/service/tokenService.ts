4086c4987e29bdf2ff7a0745ad05d1f819799b54
Vincent <nacen.dev@gmail.com>
2022-06-03 18:24:10 +0800

    feat: findToken query

--- src/service/tokenService.ts
+++ src/service/tokenService.ts
@@ -1,7 +1,11 @@
 import { nanoid } from "nanoid";
-import { Token, TokenModel } from "../model/tokenModel";
+import { TokenModel } from "../model/tokenModel";
 
 export const createToken = (userId: string) => {
   const token = nanoid();
   return TokenModel.create({ userId: userId, token: token });
+};
+
+export const findToken = (token: string) => {
+  return TokenModel.findOne({ token: token });
 };
