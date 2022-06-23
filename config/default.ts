113a44a7c7a15048fe535017025c86afaef16160
Vincent <nacen.dev@gmail.com>
2022-06-23 18:42:12 +0800

    refactor: moved other prop to custom env

--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,4 @@
 export default {
   port: 4000,
-  dbURI:
-    "mongodb+srv://admin:klaku@cluster0.avlad.mongodb.net/?retryWrites=true&w=majority",
   logLevel: "info",
-  accessTokenPrivateKey: "",
-  refreshTokenPrivateKey: "",
-  smtp: "",
 };
