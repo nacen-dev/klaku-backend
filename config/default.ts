b622b03e3ef0a107d72dedc0584ddbd4092a19ed
Vincent <nacen.dev@gmail.com>
2022-06-23 23:49:00 +0800

    build: add default config file

---
+++ config/default.ts
@@ -0,0 +1,18 @@
+export default {
+  accessTokenPublicKey: "",
+  accessTokenPrivateKey: "",
+  refreshTokenPublicKey: "",
+  refreshTokenPrivateKey: "",
+  smtp: {
+    host: "",
+    user: "",
+    pass: "",
+    port: "",
+    secure: "",
+  },
+  stripeSecretKey: "",
+  clientURL: "",
+  dbURI: "",
+  port: "",
+  logLevel: ""
+}
